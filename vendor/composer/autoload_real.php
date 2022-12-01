bb1eee5de81c78a91580f9915a29825be10fb3b3
ali rahimi <alirahimi818@gmail.com>
2022-12-01 17:04:26 +0100

    add minimum php version to composer

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInitf3be76de569caaa5d8513ee92d743a03
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInitf3be76de569caaa5d8513ee92d743a03', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInitf3be76de569caaa5d8513ee92d743a03', 'loadClassLoader'));
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'ali/persian-datepicker',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => NULL,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '5e17695d29f2dab1766fd70cf6dded887cee553f',
         'type' => 'component',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'ali/persian-datepicker' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => NULL,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '5e17695d29f2dab1766fd70cf6dded887cee553f',
             'type' => 'component',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
