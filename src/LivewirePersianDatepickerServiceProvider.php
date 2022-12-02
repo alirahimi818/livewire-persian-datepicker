d1ea0b014241e6f58140e4c9695216c129250f38
ali rahimi <alirahimi818@gmail.com>
2022-12-02 11:47:41 +0100

    publish command bug fixed

--- src/LivewirePersianDatepickerServiceProvider.php
+++ src/LivewirePersianDatepickerServiceProvider.php
@@ -25,13 +25,13 @@ class LivewirePersianDatepickerServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->commands([
-            PublishComponent::class,
-        ]);
-
         $this->publishes([
             __DIR__ . '/../resources/views/components/persian-datepicker.blade.php' => resource_path('views/components/persian-datepicker.blade.php'),
             __DIR__ . '/../resources/js/livewire-datepicker-datepicker.js' => resource_path('js/livewire-datepicker-datepicker.js'),
         ], 'livewire-persian-datepicker');
+
+        $this->commands([
+            PublishComponent::class,
+        ]);
     }
 }
